3a997fc3304b6d4e1de0d45550b39320e468ec82
Haukur Breki Hilmarsson <HaukurBreki@Haukurs-MacBook-Pro.local>
2018-09-04 10:23:37 +0000

    Algorithm code written

--- max_int.py
+++ max_int.py
@@ -1,2 +1,10 @@
-# recieve input from user
-#calculate the maximum positive integer that user inputed
+max_int = 0
+num_int = 0
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int > max_int:
+        max_int = num_int
+    
+print("The maximum is", max_int)  
+   # Do not change this line
+ 
